94595df4e44ddf96daed301512a5828d437c815b
binderclip <binderclip2014@gmail.com>
2016-03-16 13:30:06 +0800

    simple api control version

--- app.py
+++ app.py
@@ -1,15 +1,49 @@
-from flask import Flask
+# coding: utf-8
+from flask import Flask, request
+from curtain_rc import rc
 app = Flask(__name__)
+
+
+help_prompt = '''
+<html>
+<head>
+    <title>沙和尚</title>
+</head>
+<body>
+<pre>
+    ab: 'A' or 'B'，区分两个遥控，遥控背面有写 A B
+    number: 被控制的窗帘的编号，1 - 16 为单个，0 为全部
+    op: 操作
+        'close'，相当于遥控的‘上’
+        'stop'，相当于遥控的‘停’
+        'open'，相当于遥控的‘下’
+
+    向 http://cl.xcf.io:8080/clrc 发送 JSON Post
+
+    例子：
+
+    {
+        "ab": "B",
+        "number": "7",
+        "op": "stop"
+    }
+</pre>
+</body>
+</html>
+'''
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return help_prompt
 
 
-@app.route('/rc', methods=['POST'])
-def rc():
-    return 'rc'
+@app.route('/clrc', methods=['POST'])
+def clrc():
+    data = request.get_json()
+    print(data)
+    rc(data['ab'], data['number'], data['op'])
+    return 'OK'
 
 
 if __name__ == '__main__':
